50159fca31db112330e2cc9ae8fa114a3074755c
Anais <make@makes-iMac.local>
2020-04-30 16:28:03 +0100

    Added bootstrap and reactstrap and created navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Home Page</h1>
   </Layout>
 )
